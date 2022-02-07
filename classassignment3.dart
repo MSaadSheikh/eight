32f7764981dc3afd56be0890fa9e2470e9c256cc
MSaadSheikh <95358942+MSaadSheikh@users.noreply.github.com>
2022-02-07 04:54:52 -0800

    Add files via upload

---
+++ classassignment3.dart
@@ -0,0 +1,67 @@
+
+import 'dart:io';
+
+// Power(p1,p2){
+//   print(p1); //2
+//   print(p2);//5
+//   2^5=>2*2*2*2*2=32
+  // num sm=1;
+  // 1*2=2
+  // 2*2=4
+  // 4*2=8
+  // 8*2=16
+  // 16*2=32
+  // 2**5=32
+  // for(var p=1;p<=p2;p++){
+  //   sm = sm*p1;
+
+
+  // }
+  // print(sm);
+
+// }
+// void main() {
+
+// //   int a=int.parse(stdin.readLineSync()!);  //2
+// //   int b=int.parse(stdin.readLineSync()!);//5
+
+// //   Power(a,b);
+// //   // var i=1;i<b
+
+//   // if (a==b) {
+    
+//   // }
+
+
+// stdout.write("Enter your Firstname :");
+// int firstname=int.parse(stdin.readLineSync()!);
+// stdout.write("Enter your Lastname :");
+// int lastname=int.parse(stdin.readLineSync()!);
+// // HelloPrint(lname,fname);   //function Call
+// // HelloName();
+// // HelloName();
+// // HelloName();
+// // HelloName();
+// // HelloName();
+// var a = HelloName(lastname,firstname);
+// print("new :$a");
+
+// // 2^5=32
+
+
+
+// }
+
+// //  HelloName(a,b3){
+// //    print("Hello $a $b3");
+
+// // }
+// HelloName(A,B){
+//     return "hello $A $B";
+
+
+// }
+
+
+
+
